4213e200c19c22bae035e6e62f0252e1cce99fe1
unlimitbladeworks <13121347080@sina.cn>
2018-09-29 11:28:14 +0800

    update time config

--- run.py
+++ run.py
@@ -22,10 +22,9 @@ from monitor.monitor import Monitor
 from mail.send_mails import SendMail
 from config.init_configs import init_config, get_linux_config
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+init_config()  # 初始化配置
 
 def auto_job():
-    init_config()  # 初始化配置
     linux_dict = get_linux_config()
     content_list = []
     """ [(10.10.111.1,cd $(locate taskmngdomain1/nohuplogs) ; tail -50 *log*`date +%F`*),
@@ -48,16 +47,30 @@ def auto_job():
 
 
 def main():
-    # 基于quartz的定时任务调度器
+    """ 基于quartz的定时任务调度器 """
     scheduler = BlockingScheduler()
-    """ FIELD_NAMES = ('year', 'month', 'day', 'week', 'day_of_week', 'hour', 'minute', 'second') """
-    # 没用过quartz的同学去了解下CRON表达式~
-    scheduler.add_job(auto_job, 'cron', hour='0/1', id='auto_job_id')
+    """ FIELD_NAMES = ('year', 'month', 'day', 'week', 'day_of_week', 'hour', 'minute', 'second') 
+        没用过quartz的同学去了解下CRON表达式~文档如下:
+        https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html#module-apscheduler.triggers.cron
+    """
+    year = str(gol.get_value('year') or None)
+    month = str(gol.get_value('month') or None)
+    day = str(gol.get_value('day') or None)
+    week = str(gol.get_value('week') or None)
+    day_of_week = str(gol.get_value('day_of_week') or None)
+    hour = str(gol.get_value('hour') or None)
+    minute = str(gol.get_value('minute') or None)
+    second = str(gol.get_value('second') or None)
+    scheduler.add_job(auto_job, 'cron', year=year, month=month, day=day,
+                      week=week, day_of_week=day_of_week, hour=hour, minute=minute, second=second, id='auto_job_id')
     try:
+        print('程序开始!-------->Job running(Tips: Ctrl + c 终止程序)')
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
+        print('退出程序!')
         scheduler.remove_job('auto_job_id')
 
 
 if __name__ == '__main__':
     main()
+    # auto_job()
